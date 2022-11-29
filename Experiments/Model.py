46ef724b7a27f0f69b23d2c1dfa234f8de9e6bf8
yarlermanden <mmho@itu.dk>
2022-11-29 10:56:52 +0100

    Added some visualizations. Started playing with EvoTorch

--- Experiments/Model.py
+++ Experiments/Model.py
@@ -6,15 +6,7 @@ import torch.nn.functional as F
 class Complex_CA(nn.Module):
     def __init__(self, device, batch_size):
         super(Complex_CA, self).__init__()
-        #self.conv1 = nn.Conv2d(1, 1, 3, padding=1) # food spread smell
-        #self.conv2 = nn.Conv2d(4, 8, 3, padding=1) # CA perceive world
-        #self.conv3 = nn.Conv2d(8, 8, 1, padding=0) # use hidden parameters
-        #self.conv4 = nn.Conv2d(8, 4, 1, padding=0) #TODO could try replacing this to 3 and add on the last layer later
         self.device = device
-
-        self.conv3 = nn.Conv2d(12, 64, 1, padding=0) # use hidden parameters
-        self.conv4 = nn.Conv2d(64, 4, 1, padding=0) #TODO could try replacing this to 3 and add on the last layer later
-        self.conv5 = nn.Conv2d(12, 4, 1, padding=0)
         self.fc1 = nn.Linear(12, 64)
         self.fc2 = nn.Linear(64, 4)
         self.batch_size = batch_size
@@ -35,7 +27,6 @@ class Complex_CA(nn.Module):
                 torch.nn.init.zeros_(m.bias)
         with torch.no_grad():
             self.apply(init_weights)
-        self.alive = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
 
     def perceive_scent(self, food):
         food = food.view(self.batch_size,1,17,17)
@@ -43,17 +34,13 @@ class Complex_CA(nn.Module):
         return x
 
     def alive_filter(self, x):
-        return self.alive(x[:, 0:1, :, :]) > 0.1
+        alive = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        return alive(x[:, 0:1, :, :]) > 0.1
 
-    def live_count_above(self, x, threshold):
+    def living_cells_above(self, x, threshold):
         return (x > threshold).to(torch.float).sum(dim=(1,2,3))
 
-    def alive_count(self, x):
-        #TODO maybe best to only look at nearest x neighbors and not total count in case we make a big world
-        #x = torch.stack([x])
-        #conv_weights = torch.ones_like(x)
-        #live_count = F.conv2d(x, conv_weights)[0][0]
-
+    def total_pixel_val(self, x):
         live_count = x.sum(dim=(1,2,3))
         return live_count
 
@@ -65,67 +52,39 @@ class Complex_CA(nn.Module):
         y1 = _perceive_with(x, self.dx)
         y2 = _perceive_with(x, self.dy)
         y = torch.cat((x,y1,y2), dim=1) 
-        #y = torch.relu(self.conv2(x)) #perceive neighbor cell state
         return y
 
     def update(self, cell, food):
         #TODO: handle somewhere in some way if food is reached and consumed. Remove food and increase CA size
 
         x = cell
-
         x[:, 3] = self.perceive_scent(food) #update scent 
 
         pre_life_mask = self.alive_filter(x)
         x = self.perceive_cell_surrounding(x) #perceive neighbor cell states
 
-        #perform NN for single cell
-        #x = torch.relu(self.conv3(x)) #single cell 
-        #x = self.conv4(x) #single cell
-
-        #x.shape = 16, 17, 17, 12 - B, W, H, C - batch, width, height, channel
-        #iterate each batch, width and height:
         x = x.transpose(1, 3)
         x = torch.relu(self.fc1(x))
         x = self.fc2(x)
         x = x.transpose(1, 3)
-        #print(x.shape)
-        #x = self.conv5(x) #single cell
-
 
         x = cell + x
-
-        #force harder boundaries - is this necessary when the other two masks almost do the same?
-        #enabling this will cause the model to never move
-        #threshold_mask = (x[:, 0] > 0.1).to(torch.float) #ensures cells have to be more than a certain amount alive to count - ensures harder boundaries
-        #x[:, 0] = x[:, 0]*threshold_mask
-
-        #Force into range between 0 and 1
-        # Could replace with function that only allows 0 or 1 - argmax
-        #x[:, 0] = torch.sigmoid(x[:, 0])
-        #x[:, 0] = F.softmax(x[:, 0], dim=1) #softmax definitely won't work as it needs 2 outputs representing 0 or 1 for each cell to be softmaxed between...
 
         post_life_mask = self.alive_filter(x) #only do this on cell state
         life_mask = torch.bitwise_and(pre_life_mask, post_life_mask).to(torch.float)
         x = x*life_mask
 
-        #x[0] = torch.relu(x[0]) #force all cells to be between 0 and 1
         x = torch.clamp(x, -10.0, 10.0)
-        
 
         #TODO we need some way of ensuring cells close to each other have a much much much lower cost difference than cells far from each other...
         #TODO definely need some better way of measuring loss...
         #TODO could we measure loss from the center of the cell aswell - to reward for being close to the actual representation
-
         return x, food
         
-
     def forward(self, cell: torch.Tensor, food: torch.Tensor, steps: int):
         for _ in range(steps):
             cell, food = self.update(cell, food)
 
-        #TODO could maybe even add count of cells above some threshold e.g. 0.9 - force completely living cells
-        living_count = self.alive_count(cell[:, 0:1])
-        #TODO could also try with a much lower threshold 
-        # living_count should force close to equal amount of total pixel values while a low threshold ensures no cells with smaller values as these would quickly add up
-        living_above = self.live_count_above(cell[:, 0:1], 0.1)
-        return cell, food, living_count, living_above
+        total_pixel_val = self.total_pixel_val(cell[:, 0:1])
+        living_count = self.living_cells_above(cell[:, 0:1], 0.1)
+        return cell, food, total_pixel_val, living_count
