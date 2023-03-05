1be16a2ed1830af2d593a7525224acbe90801207
yarlermanden <mmho@itu.dk>
2023-03-05 22:35:47 +0100

    Changed up a lot of the convolutions to try different stuff. Made a new type of custom message passing for better suiting important information being stored in the edges.

--- Thesis/src/CGConv.py
+++ Thesis/src/CGConv.py
@@ -1,28 +1,32 @@
 from GNCAmodel import GNCA
-from torch_geometric.nn import CGConv
+from torch_geometric.nn import CGConv, NNConv
 from torch_geometric_temporal.nn.recurrent import gconv_gru
 import torch.nn as nn
 import torch
 
+from custom_conv import CustomConv, CustomConvSimple
 
 class CGConv1(GNCA):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.input_channels = 2
         self.output_channels = 2
-        self.hidden_size = self.input_channels*2
-        self.conv_layer_cells = CGConv(self.hidden_size, dim=self.edge_dim, aggr='mean')
-        self.conv_layer_food = CGConv(self.hidden_size, dim=self.edge_dim, aggr='mean')
+        self.hidden_size = self.input_channels*1
+        #self.conv_layer_cells = CGConv(self.hidden_size, dim=self.edge_dim, aggr='mean')
+        #self.conv_layer_food = CGConv(self.hidden_size, dim=self.edge_dim-1, aggr='add')
+        #self.conv_layer_food_neg = CGConv(self.hidden_size, dim=self.edge_dim-1, aggr='add')
+        
+        self.conv_layer_food = CustomConvSimple(self.hidden_size, dim=self.edge_dim-1, aggr='add')
+        self.conv_layer_cell = CustomConvSimple(self.hidden_size, dim=self.edge_dim-1, aggr='add')
 
         self.mlp_before = nn.Sequential(
             #nn.Linear(self.input_channels, self.input_channels*2),
-            #nn.ReLU(),
+            #nn.Tanh(),
             nn.Linear(self.input_channels, self.hidden_size),
             nn.Tanh(),
         )
 
         self.mlp_middle = nn.Sequential(
-            nn.Tanh(),
             nn.Linear(self.hidden_size*2, self.hidden_size),
             nn.Tanh(),
         )
@@ -44,7 +48,49 @@ class CGConv1(GNCA):
         self.H = None
         #self.C = None
 
+        self.sigmoid = nn.Sigmoid()
+        self.tanh = nn.Tanh()
+
+        self.mlp_edge = nn.Sequential(
+            nn.Linear(3,3),
+            nn.Tanh(),
+            nn.Linear(3,3),
+            nn.Tanh(),
+        )
+
     def message_pass(self, graph):
+        food_edges = graph.edge_index[:, torch.nonzero(graph.edge_attr[:, 3] == 0).flatten()]
+        food_attr = graph.edge_attr[torch.nonzero(graph.edge_attr[:, 3] == 0).flatten()][:, :3]
+        cell_edges = graph.edge_index[:, torch.nonzero(graph.edge_attr[:, 3] == 1).flatten()]
+        cell_attr = graph.edge_attr[torch.nonzero(graph.edge_attr[:, 3] == 1).flatten()][:, :3]
+
+        x = graph.x[:, 2:4] * 50
+        food_attr *= 4
+        cell_attr *= 4
+        
+        #food_attr = self.mlp_edge(food_attr)
+        x = self.mlp_before(x)
+        x_food = self.conv_layer_food(x=x, edge_index=food_edges, edge_attr=food_attr)
+        x_cell = self.conv_layer_cell(x=x, edge_index=cell_edges, edge_attr=cell_attr)
+        x = x_food + x_cell
+
+        #x_pos = self.conv_layer_food(x=x, edge_index=food_edges, edge_attr=food_attr)
+        #x_neg = self.conv_layer_food_neg(x=x, edge_index=food_edges, edge_attr=food_attr)
+        #x = x + x_pos - x_neg
+
+        #x = self.mlp(x)
+
+        #x = torch.concat((x_pos, -x_neg), dim=1)
+        if self.H is None:
+            self.H = torch.zeros_like(x, device=self.device)
+        if self.node_indices_to_keep is not None:
+            self.H = self.H[self.node_indices_to_keep].view(self.node_indices_to_keep.shape[0], self.H.shape[1])
+        self.H = self.tanh(self.gConvGRU(x, food_edges, H=self.H))
+        x = x + self.H
+
+        #x = self.mlp_middle(x)
+        return x
+
         #x = self.mlp_before(torch.concat((graph.x[:, 2:4], graph.x[:, 5:7]), dim=1))
         x = self.mlp_before(graph.x[:, 2:4])
 
---
+++ Thesis/src/custom_conv.py
@@ -0,0 +1,113 @@
+from typing import Tuple, Union
+
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+from torch.nn import BatchNorm1d, Linear
+from torch import nn
+
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.typing import Adj, OptTensor, PairTensor
+
+class CustomConv(MessagePassing):
+    def __init__(self, channels: Union[int, Tuple[int, int]], dim: int = 0,
+                aggr: str = 'add', batch_norm: bool = False,
+                bias: bool = True, **kwargs):
+        super().__init__(aggr=aggr, **kwargs)
+        self.channels = channels
+        self.dim = dim
+        self.batch_norm = batch_norm
+
+        if isinstance(channels, int):
+            channels = (channels, channels)
+
+        self.lin_f = Linear(sum(channels) + dim, channels[1], bias=bias)
+        self.lin_s = Linear(sum(channels) + dim, channels[1], bias=bias)
+        if batch_norm:
+            self.bn = BatchNorm1d(channels[1])
+        else:
+            self.bn = None
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.lin_f.reset_parameters()
+        self.lin_s.reset_parameters()
+        if self.bn is not None:
+            self.bn.reset_parameters()
+
+    def forward(self, x: Union[Tensor, PairTensor], edge_index: Adj,
+                edge_attr: OptTensor = None) -> Tensor:
+        """"""
+        if isinstance(x, Tensor):
+            x: PairTensor = (x, x)
+
+        # propagate_type: (x: PairTensor, edge_attr: OptTensor)
+        out = self.propagate(edge_index, x=x, edge_attr=edge_attr, size=None)
+        out = out if self.bn is None else self.bn(out)
+        out = out + x[1]
+        return out
+
+    def message(self, x_i, x_j, edge_attr: OptTensor) -> Tensor:
+        if edge_attr is None:
+            z = torch.cat([x_i, x_j], dim=-1)
+        else:
+            z = torch.cat([x_i, x_j, edge_attr], dim=-1)
+        return self.lin_f(z).sigmoid() * F.tanh(self.lin_s(z))
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({self.channels}, dim={self.dim})'
+
+class CustomConvSimple(MessagePassing):
+    def __init__(self, channels: Union[int, Tuple[int, int]], dim: int = 0,
+                aggr: str = 'add', batch_norm: bool = False,
+                bias: bool = True, **kwargs):
+        super().__init__(aggr=aggr, **kwargs)
+        self.channels = channels
+        self.dim = dim
+        self.batch_norm = batch_norm
+
+        if isinstance(channels, int):
+            channels = (channels, channels)
+
+        self.lin = Linear(sum(channels) + dim, channels[1], bias=bias)
+        input = sum(channels) + dim
+        self.mlp = nn.Sequential(
+            Linear(input, input),
+            nn.Tanh(),
+            Linear(input, channels[1])
+        )
+        if batch_norm:
+            self.bn = BatchNorm1d(channels[1])
+        else:
+            self.bn = None
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.lin.reset_parameters()
+        if self.bn is not None:
+            self.bn.reset_parameters()
+
+    def forward(self, x: Union[Tensor, PairTensor], edge_index: Adj,
+                edge_attr: OptTensor = None) -> Tensor:
+        """"""
+        if isinstance(x, Tensor):
+            x: PairTensor = (x, x)
+
+        # propagate_type: (x: PairTensor, edge_attr: OptTensor)
+        out = self.propagate(edge_index, x=x, edge_attr=edge_attr, size=None)
+        out = out if self.bn is None else self.bn(out)
+        out = out + x[1]
+        return out
+
+    def message(self, x_i, x_j, edge_attr: OptTensor) -> Tensor:
+        if edge_attr is None:
+            z = torch.cat([x_i, x_j], dim=-1)
+        else:
+            z = torch.cat([x_i, x_j, edge_attr], dim=-1)
+        #return torch.tanh(self.lin(z))
+        return torch.tanh(self.mlp(z))
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({self.channels}, dim={self.dim})'
