50578bd9179df6116880530d0c3f96bd67b7820f
yarlermanden <mmho@itu.dk>
2023-04-10 15:49:09 +0200

    tried implementing a way of saving and loading searchers to continue training on previous sessions, but it doesn't work yet

--- Thesis/src/evo_trainer.py
+++ Thesis/src/evo_trainer.py
@@ -7,6 +7,8 @@ import numpy as np
 import ray
 import time
 import math
+import pickle as Pickle
+import dill
 
 from GNCAConv import Conv
 from global_state import GlobalState
@@ -17,12 +19,12 @@ class Evo_Trainer():
     def __init__(self, settings):
         cpu = torch.device('cpu')
         self.settings = settings
-        global_var = GlobalState.remote(settings)
-        ray.get(global_var.set_global_var.remote())
+        self.global_var = GlobalState.remote(settings)
+        ray.get(self.global_var.set_global_var.remote())
 
         self.problem = Custom_NEProblem(
             settings=settings,
-            global_var=global_var,
+            global_var=self.global_var,
             device=cpu,
             objective_sense=['max', 'max', 'max', 'max'],
             network=Conv,
@@ -49,15 +51,15 @@ class Evo_Trainer():
                 limit_C_decomposition=False,
                 obj_index=0,
             )
-
-        def before_epoch():
-            ray.get(global_var.set_global_var.remote())
             
-        self.searcher.before_step_hook.append(before_epoch)
+        self.searcher.before_step_hook.append(self.before_epoch)
         self.logger = StdOutLogger(self.searcher)
         self.logger = PandasLogger(self.searcher)
         self.logger_df = None
         self.trained_network = Conv(settings=settings)
+
+    def before_epoch(self):
+        ray.get(self.global_var.set_global_var.remote())
 
     def parameterize_net(self):
         if self.settings.train_config.problem_searcher == ProblemSearcher.GeneticAlgorithm:
@@ -65,7 +67,9 @@ class Evo_Trainer():
         else:
             self.trained_network = self.problem.parameterize_net(self.searcher.status['center'][0])
 
-    def train(self, n=1000, name='test1'):
+    def train(self, n=1000, name=None):
+        if name is None:
+            name = self.settings.train_config.name
         n1 = n
         t = math.ceil(n/1000)
         for _ in range(t):
@@ -76,6 +80,7 @@ class Evo_Trainer():
             self.parameterize_net()
             torch.save(self.trained_network.state_dict(), '../models/' + name + '.pth')
             n1 -= x
+        #self.save_searcher(name)
 
     def visualize_training(self):
         logger_df = self.logger_df.groupby(np.arange(len(self.logger_df))).mean()
@@ -84,6 +89,32 @@ class Evo_Trainer():
     def get_trained_network(self):
         self.parameterize_net()
         return self.trained_network
+
+    def save_searcher(self, name=None):
+        problem = self.searcher.problem
+        if name is None:
+            name = self.settings.train_config.name
+        with open(r'../searcher/' + name + '.pkl', 'wb') as file:
+            dic = self.searcher.__dict__.copy()
+            print(dic)
+            del dic['problem']
+            dill.dump(dic, file)
+            #Pickle.dump(self.searcher, file)
+            #pd.to_pickle(self.searcher, file)
+
+    def load_searcher(self, name=None):
+        if name is None:
+            name = self.settings.train_config.name
+        with open(r'../searcher/' + name + '.pkl', 'rb') as file:
+            #dic = Pickle.load(file)
+            searcher = dill.load(file)
+            searcher.problem = self.problem
+            self.searcher = searcher
+
+            #if self.settings.train_config.problem_searcher == ProblemSearcher.GeneticAlgorithm:
+            #    self.searcher = GeneticAlgorithm(**dic)
+            #else:
+            #    self.searcher = CMAES(**dic)
 
 #TODO add something for actually saving the problem in such a way we can reinitialize it and continue
 #possibly we could also change the settings in regarding to change the population and such in the population
