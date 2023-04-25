55b7c459d2f8a23d27012ccca5a69701c618734f
yarlermanden <mmho@itu.dk>
2023-04-25 09:00:34 +0200

    fixed bug in creation of Global models

--- Thesis/src/organism.py
+++ Thesis/src/organism.py
@@ -34,12 +34,13 @@ class Organism():
         '''transforms all cells in organism to nodes in a graph'''
         hidden = [0, 0, 0, 0, 0]
         x = torch.tensor([[cell.pos[0], cell.pos[1], cell.vel[0], cell.vel[1], 1, 50, *hidden] for cell in self.cells], device=self.device)
-        if self.settings.model_type == ModelType.SmallWorld:
-            x[:self.settings.n2, 4] = NodeType.LongRadiusCell
-        elif self.settings.model_type == ModelType.WithGlobalNode: add_global_node(graph, self.device)
-
         edges = torch.tensor([[]], device=self.device)
         graph = Data(x=x, edge_index=edges, device=self.device, subsize=len(x))
+
+        if self.settings.model_type == ModelType.SmallWorld:
+            graph.x[:self.settings.n2, 4] = NodeType.LongRadiusCell
+        elif self.settings.model_type == ModelType.WithGlobalNode: add_global_node(graph, self.device)
+
 
         random_number = random.randint(0, 3)
         food_env = self.settings.food_envs[random_number]
