26f112695befb3572f8b3b67a4aab58a6b4ec3a8
yarlermanden <mmho@itu.dk>
2023-04-25 09:01:57 +0200

    made conv faster by only doing mlp on cells as rest should be irrelevant

--- Thesis/src/GNCAConv.py
+++ Thesis/src/GNCAConv.py
@@ -7,17 +7,13 @@ from torch_geometric.nn.norm import pair_norm
 
 from custom_conv import *
 from enums import *
+from graphUtils import *
 
 class Conv(GNCA):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.velNorm = 1.0*self.settings.scale/self.max_velocity
         self.attrNorm = 1.0*self.settings.scale/self.settings.radius_food
-        
-        #self.mlp_before = nn.Sequential(
-        #    nn.Linear(self.input_channels, self.hidden_size),
-        #    nn.Tanh(),
-        #)
 
         self.hidden_after_size = self.hidden_size + 2 + 2
         if self.model_type == ModelType.WithGlobalNode: self.hidden_after_size += self.hidden_size
@@ -35,20 +31,21 @@ class Conv(GNCA):
         self.conv_layer_food = CustomConvEdgeOnly(2, dim=self.edge_dim-1, aggr='mean')
         self.conv_layer_wall = CustomConvEdgeOnly(2, dim=self.edge_dim-1, aggr='mean')
         if self.model_type == ModelType.WithGlobalNode:
+            #TODO consider making the global convolution to GCN as we simply want message passing through this and thus smoothening node features
             self.conv_layer_global = CustomConvSimple(self.hidden_size, dim=self.edge_dim-1, aggr='mean')
-        #self.gConvGRU = gconv_gru.GConvGRU(in_channels=self.hidden_after_size, out_channels=self.hidden_after_size, K=1).to(self.device)
+        #self.gConvGRU = gconv_gru.GConvGRU(in_channels=2, out_channels=2, K=1).to(self.device)
 
         self.H = None
         self.pair_norm = pair_norm.PairNorm()
         for param in self.parameters():
             param.grad = None
 
-    def gru(self, graph, x):
+    def gru(self, edges, x):
         if self.H is None:
             self.H = torch.zeros_like(x, device=self.device)
         if self.node_indices_to_keep is not None:
             self.H = self.H[self.node_indices_to_keep].view(self.node_indices_to_keep.shape[0], self.H.shape[1])
-        self.H = torch.tanh(self.gConvGRU(x, graph.edge_index, H=self.H))
+        self.H = torch.tanh(self.gConvGRU(x, edges, H=self.H))
         return self.H
 
     def message_pass(self, graph):
@@ -61,6 +58,7 @@ class Conv(GNCA):
             global_attr = graph.edge_attr[torch.nonzero(graph.edge_attr[:, 3] == 2).flatten()][:, :3]
         wall_edges = graph.edge_index[:, torch.nonzero(graph.edge_attr[:, 3] == 4).flatten()]
         wall_attr = graph.edge_attr[torch.nonzero(graph.edge_attr[:, 3] == 4).flatten()][:, :3]
+        c_mask = cell_mask(graph.x)
 
         energy_norm = graph.x[:, 5:6] * 0.01
         x_origin = torch.concat((graph.x[:, 2:4] * self.velNorm, energy_norm, graph.x[:, 6:]), dim=1)  #vel, energy, hidden
@@ -68,7 +66,6 @@ class Conv(GNCA):
         cell_attr *= self.attrNorm
         wall_attr *= self.attrNorm
         
-        #x = self.mlp_before(x_origin)
         x = x_origin
         x_food = self.conv_layer_food(x=x[:, :2], edge_index=food_edges, edge_attr=food_attr)
         x_cell = self.conv_layer_cell(x=x, edge_index=cell_edges, edge_attr=cell_attr)
@@ -81,26 +78,22 @@ class Conv(GNCA):
         else: x = torch.concat((x_food, x_cell, x_wall), dim=1)
         x = torch.tanh(x)
 
-        #x = self.gru(graph, x)
-        x = self.mlp_after(x)
+        output = torch.zeros((x.shape[0], self.output_channels), device=self.device)
+        output[c_mask] = self.mlp_after(x[c_mask])
+        x = output
+        #x[:, :2] += self.gru(cell_edges, x[:, :2])
+
         #... and normalize hidden features H
-        #x[:, 2:] = torch.tanh(x[:, 2:]/10 + x_origin[:, 3:]/(4/3))
-        x[:, 2:] = torch.tanh(pair_norm(x[:, 2:] + x_origin[:, 3:]))
+        x[c_mask, 2:] = torch.tanh(x[c_mask, 2:]/10 + x_origin[c_mask, 3:]*0.75)
+        #x[:, 2:] = torch.tanh(self.pair_norm(x[:, 2:] + x_origin[:, 3:]))
+        #x[:, 2:] = self.pair_norm(x[:, 2:] + x_origin[:, 3:])
+        #x[c_mask, 2:] = self.pair_norm.forward(x[c_mask, 2:] + x_origin[c_mask, 3:]) #TODO compute this correctly
 
-
-
-        #don't make residual connections as we don't want momentum to carry over or values to be able to go beyond/below 1/-1 
-        #simply persist should result in staying still and not moving...
-        #x[:, :2] += x_origin[:, :2] #only the velX and velY should be kept
-
-        #x += torch.concat((x_origin[:, :2], x_origin[:, 3:]), dim=1)
-        #x = torch.tanh(x) #enforces values between -1 and 1 - furthermore forces values a bit lower than otherwise, meaning if nothing is added, it will decrease with time - hopefully less exploding values
-        return x
+        return x 
 
     def forward(self, *args):
         self.H = None
         self.node_indices_to_keep = None
-        #self.mlp_before = self.mlp_before.to(self.device)
         self.conv_layer_cell = self.conv_layer_cell.to(self.device)
         self.conv_layer_food = self.conv_layer_food.to(self.device)
         self.conv_layer_wall = self.conv_layer_wall.to(self.device)
