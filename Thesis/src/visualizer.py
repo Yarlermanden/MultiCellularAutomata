adc5a278151a09e031066e1b1848c254f2f4836a
yarlermanden <mmho@itu.dk>
2023-04-25 09:03:42 +0200

    Made gif length depend on the organisms live time. should end 10 timesteps after all cells die. Added plotting of graph as graph and not organism.

--- Thesis/src/visualizer.py
+++ Thesis/src/visualizer.py
@@ -3,6 +3,10 @@ import matplotlib.animation as animation
 import numpy as np
 import torch
 import math
+import random
+import networkx as nx
+from torch_geometric.utils import to_networkx
+from torch_geometric.data import Data
 
 from datastructure import DataStructure
 from graphUtils import add_random_food, cell_mask, food_mask, wall_mask
@@ -105,17 +109,81 @@ class Visualizer():
             self.figure.canvas.flush_events()
             s_idx = e_idx
 
-    def animate_organism(self, graph, model, food=100, frames=50, interval=150):
+    def animate_organism(self, graph, model, interval=150):
         self.graph = graph
         self.plot_organism(graph.clone().detach().cpu())
 
-        @torch.no_grad()
-        def animate(i):
-            self.graph = model.update(self.graph)
-            self.plot_organism(self.graph.detach().cpu())
+        def gen(): #plot until 10 frames after all cells die
+            count_down = 10
+            while count_down > 0:
+                graph = model.update(self.graph)
+                self.graph = graph
+                if cell_mask(self.graph.x).sum() == 0: #no more cells
+                    count_down -= 1
+                yield graph
 
-        anim = animation.FuncAnimation(self.figure, animate, frames=frames, interval=interval)
+        @torch.no_grad()
+        def animate(graph):
+            self.plot_organism(graph.detach().cpu())
+
+        anim = animation.FuncAnimation(self.figure, animate, frames=gen, interval=interval, save_count=2000)
         return anim
 
     def save_animation_to_gif(self, anim, name, fps=30, dpi=200):
         anim.save('../animation/' + name + '.gif', writer='imagemagick', fps=fps, dpi=dpi)
+
+    def plot_graph(self, graph):
+        '''
+        Plot a graph in a tree-like structure, where the root node is the news article.
+        '''
+        #any_edges = self.datastructure.add_edges(graph)
+        nodes2 = graph.x[:graph.subsize[0]]
+        #edges2 = graph.edge_index[graph.subsize[0]]
+        #edges2 = graph.edge_index[torch.nonzero(torch.isin(graph.edge_index[0], torch.nonzero(nodes2))).view(-1)]
+        edges2 = graph.edge_index[:, torch.nonzero(graph.edge_index[0] < graph.subsize[0])].squeeze()
+        graph2 = Data(nodes2, edges2)
+        G = to_networkx(graph2)
+        N = G.number_of_nodes()
+
+        figure, axes = plt.subplots(1, 1, figsize=(10,5))
+
+        nodes = G.nodes()
+
+        #different node types:
+        cells = torch.nonzero(cell_mask(graph2.x)).flatten()
+        food = torch.nonzero(food_mask(graph2.x)).flatten()
+        walls = torch.nonzero(wall_mask(graph2.x)).flatten()
+        
+        root = cells[0]
+
+        # Define positions to draw hierarchical diagram. See pydoc for source.
+        #pos = self.hierarchy_pos(G=G, root=root)
+        #pos = nx.spring_layout(G)
+        pos = nx.shell_layout(G)
+
+        # Define the colors
+        node_color = [None] * N
+        for cell_idx in cells:
+            node_color[cell_idx] = 'tab:blue'
+        for food_idx in food:
+            node_color[food_idx] = 'tab:red'
+        for wall_idx in walls:
+            node_color[wall_idx] = 'tab:green'
+
+        # Define node sizes
+        node_sizes = [1e3 // N] * N
+        node_sizes[root] = node_sizes[0]*2
+
+        #nx.draw_networkx_nodes
+        nx.draw(
+        #nx.draw_networkx_nodes(
+        #nx.draw_networkx(
+            G=G, 
+            pos=pos, 
+            node_color=node_color, 
+            #labels={root: 'Root'},
+            edge_color= 'tab:gray',
+            node_size=node_sizes,
+            width=[0.3],
+            ax=axes
+        )
