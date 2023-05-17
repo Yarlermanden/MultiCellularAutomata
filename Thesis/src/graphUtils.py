fixed two minor bugs in degree calculation

Thesis/src/graphUtils.py
@@ -152,12 +152,13 @@ def wall_damage(graph, settings):
         c_edges_below_distance = torch.bitwise_and(graph.edge_attr[:, 3] == EdgeType.CellToCell, graph.edge_attr[:, 0] < settings.radius)
         cell_edge_indices = torch.nonzero(c_edges_below_distance).flatten()
         cell_edge_count = torch.bincount(graph.edge_index[1, cell_edge_indices], minlength=graph.x.shape[0])
-        graph.x[cell_indices, 5] -= settings.wall_damage / torch.clamp(cell_edge_count[cell_indices], max=settings.max_degree)
+        graph.x[cell_indices, 5] -= settings.wall_damage / torch.clamp(cell_edge_count[cell_indices], min=1, max=settings.max_degree)
 
 def degree_below_radius(graph, settings):
-    edge_below_distance = torch.nonzero(graph.edge_attr[:, 0] < settings.radius).flatten() #TODO should be able to optimize this the same way as with walls - to don't bin count all other type of edges
+    c_edges_below_distance = torch.bitwise_and(graph.edge_attr[:, 3] == EdgeType.CellToCell, graph.edge_attr[:, 0] < settings.radius)
+    edge_below_distance = torch.nonzero(c_edges_below_distance).flatten()
     degree = torch.bincount(graph.edge_index[1, edge_below_distance], minlength=graph.x.shape[0])
-    return torch.clamp(degree, max=settings.max_degree)
+    return torch.clamp(degree, min=1, max=settings.max_degree)
 
 def get_dead_cells_mask(graph, energy_required):
     '''Returns mask of cells with less than required energy level'''